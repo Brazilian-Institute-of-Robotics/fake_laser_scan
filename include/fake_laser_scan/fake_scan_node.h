64077ec02f19c6c71eeed88070004dad85c6c93d
lucasmarbatista <lucasmarbatista>
2020-01-13 11:13:12 -0300

    Reconfigure files and classes

--- include/fake_laser_scan/fake_scan_node.h
+++ include/fake_laser_scan/fake_scan_node.h
@@ -1,43 +1,38 @@
-//Author: Nicolas Gallardo
-//Date:7/30/17
-//This header file defines the classes and functions required to produce and publish fake laser scans
-//a as ROS LaserScan message
+// Author: Nicolas Gallardo
+// Date:7/30/17
+// This header file defines the classes and functions required to produce and publish fake laser scans
+// a as ROS LaserScan message
 
-#ifndef FAKE_SCAN_NODE_H_
-#define FAKE_SCAN_NODE_H_
-
-//ROS includes:
+#ifndef FAKE_LASER_SCAN_FAKE_SCAN_NODE_H_
+#define FAKE_LASER_SCAN_FAKE_SCAN_NODE_H_
 
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 
-//global variables:
+// Global variables
 #define g_PI 3.14159265359
 #define g_SAMPLES_PER_REV 400
 #define g_LASER_FREQ 10
 
-class FakeLaserScan{
-	
-	public:
-	FakeLaserScan(){};
-	void begin();
-	void fakeScanPublisher();
-	
-	private:
-	float32 angle_min;
-	float32 angle_max;
-	float32 angle_increment;
-	float32 time_increment;
-	float32 scan_time;
-	float32 range_min;
-	float32 range_max;
-	float32[] ranges;
-	float32[] intensities;
-	float32 last_scan_time;
-	float32 current_scan_time;
-	
-}
+class FakeLaserScan {
+ public:
+  FakeLaserScan();
+  void begin(int argc, char **argv);
+  void fakeScanPublisher();
 
-extern FakeLaserScan laserScan;
+ private:
+  sensor_msgs::LaserScan scan;
+  float angle_min;
+  float angle_max;
+  float angle_increment;
+  float time_increment;
+  float scan_time;
+  float range_min;
+  float range_max;
+  float ranges[];
+  float intensities[];
+  float last_scan_time;
+  float current_scan_time;
+};
 
-#endif /* FAKE_SCAN_NODE_H_ */
+#endif  // FAKE_LASER_SCAN_FAKE_SCAN_NODE_H_
